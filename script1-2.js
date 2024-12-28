3203d69b1b17b5e17c91811c8bf96276f588e00a
freddyy1 <arek617@wp.pl>
2024-12-28 19:01:47 +0100

    Update script1-2.js

--- script1-2.js
+++ script1-2.js
@@ -8,58 +8,6 @@ if (typeof GAME === 'undefined') { } else {
         }
     }, 50);
 
-if (typeof GAME === 'undefined') { } else {
-    let Pog = setInterval(() => {
-        if (!GAME.pid) { } else {
-            clearInterval(Pog);
-        }
-    }, 50);
-
-    var modalStyle = document.createElement('style')
-    modalStyle.textContent = `
-     /* The Modal (background) */
-.kwaModal {
-  display: block; /* Hidden by default */
-  position: fixed; /* Stay in place */
-  z-index: 1; /* Sit on top */
-  left: 0;
-  top: 0;
-  width: 100%; /* Full width */
-  height: 100%; /* Full height */
-  overflow: auto; /* Enable scroll if needed */
-  background-color: rgb(0,0,0); /* Fallback color */
-  background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
-}
-
-/* Modal Content/Box */
-.kwaModalContent {
-  background-color: #fefefe;
-  margin: 15% auto; /* 15% from the top and centered */
-  padding: 20px;
-  border: 1px solid #888;
-  width: 80%; /* Could be more or less, depending on screen size */
-}
-
-/* The Close Button */
-.closeModalUpdate {
-  color: #aaa;
-  float: right;
-  font-size: 28px;
-  font-weight: bold;
-}
-
-.closeModalUpdate:hover,
-.closeModalUpdate:focus {
-  color: black;
-  text-decoration: none;
-  cursor: pointer;
-} 
-    `;
-    document.head.appendChild(modalStyle);
-    $("body").append(`<div id="kwaModalUpdate" class="kwaModal"> <div class="kwaModalContent"> <span class="closeModalUpdate">&times;</span> <p>Korzystasz ze starej wersji rozszerzenia. Pobierz aktualizacje z działu aktualizacje:
-        https://discord.com/invite/FCsgB6EQw9</p> </div>`);
-}
-
     let Pgg = setInterval(() => {
         clearInterval(Pgg);
         Array.from(document.getElementsByTagName('script')).forEach(script => {
@@ -2597,7 +2545,7 @@ if (typeof GAME === 'undefined') { } else {
             $('#available_servers option[value=' + this.server + ']').prop('selected', true);
         };
         const kulka = new ballManager();
-        
+        const ekwipunek = new ekwipunekMenager();
         let adimp = false;
         let arena_count = 0;
         let pvp_count = 0;
